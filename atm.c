328eaf62405180f3b4b9db925b1122c6225ee5b2
AKisliy <akisliy@gmail.com>
2017-10-22 21:30:13 +0300

    atm(bankomat)

--- atm.c
+++ atm.c
@@ -3,30 +3,36 @@
 #include <time.h>
 
 int main() {
-    int const N = 10000;
-    int const ADMIN = 1234;
-    int pincode[N];
-    int client[N];
-    int pinc;
-    char root;
+    int const NUMBER_OF_ACCOUNTS = 5000;
+    int const ADMIN_PIN = 1234;
+    int const MIN_PINCODE_VALUE = 1000;
+    int const MAX_PINCODE_VALUE = 9999;
+    int const CHECK_BALANCE = 1;
+    int const TOP_UP_THE_BALANCE = 2;
+    int const WITHDRAW_CASH = 3;
+    int const EXIT = 4;
+    int const NUMBER_OF_ATTEMPTS = 3;
+    int pincode[NUMBER_OF_ACCOUNTS];
+    int client[NUMBER_OF_ACCOUNTS];
+    int password;
     
     srand(time(NULL));
     
-    for(int i = 0; i < N; i++){
-        pincode[i] = 1000 + rand() % 8999;
+    for(int i = 0; i < NUMBER_OF_ACCOUNTS; i++){
+        pincode[i] = 1000 + rand() % 8999;  // pin codes range from 1000 to 9999
     }
      
-    for(int j = 0; j < N; j++){
-        client[j] = rand() % 1000;
+    for(int j = 0; j < NUMBER_OF_ACCOUNTS; j++){
+        client[j] = rand() % 1000;  // customer balance range from 0 to 1000
     }
     
-    for(int k = 0; k < 3; k++){
+    for(int k = 0; k < NUMBER_OF_ATTEMPTS; k++){
         for(;;){
             printf("Enter pin-code: ");
-            int newpinc = scanf("%d", &pinc);
+            int newPincode = scanf("%d", &password);
        
-            if(newpinc){
-                if((pinc < 999) || (pinc > 9999)){
+            if(newPincode){
+                if((password < MIN_PINCODE_VALUE) || (password > MAX_PINCODE_VALUE)){
                 printf("You entered an incorrect pin code\n");
                 continue;
                 }
@@ -38,29 +44,30 @@ int main() {
         break;
         }
         
-        if(pinc == ADMIN){
-            for(int j = 0; j < N; j++){
+        if(password == ADMIN_PIN){
+            char selector;
+            for(int j = 0; j < NUMBER_OF_ACCOUNTS; j++){
                 printf("%d\n", client[j]);
             }
             
-            printf("Do you want to clear(y/n) ");
-            scanf("%s", &root);
+            printf("Do you want to clear customers balance(y/n): ");
+            scanf("%s", &selector);
             
-            if(root == 'y'){
-                for(int j = 0; j < N; j++){
+            if(selector == 'y'){
+                for(int j = 0; j < NUMBER_OF_ACCOUNTS; j++){
                     client[j] = 0;
                     printf("%d\n", client[j]);
                 }
-                k = k - 1;
+                k -= 1;  //reset the number of attempts
                 continue;
             } else{
-                k = k - 1;
+                k -= 1;  //reset the number of attempts
                 continue;
             }
         }
         
-        for(int i = 0; i < N; i++){
-            if(pinc == pincode[i]){
+        for(int i = 0; i < NUMBER_OF_ACCOUNTS; i++){
+            if(password == pincode[i]){
                 for(;;){
                     int selector;
                     int operation;
@@ -72,17 +79,17 @@ int main() {
                     printf("Exit(4)\n");
                     scanf("%d", &selector);
                     
-                    if(selector == 1){
+                    if(selector == CHECK_BALANCE){
                         printf("On your account: %d\n", client[i]);
                     }
                     
-                    if(selector == 2){
+                    if(selector == TOP_UP_THE_BALANCE){
                         printf("Enter the sum: ");
                         scanf("%d", &operation);
                         client[i] = client[i] + operation;
                     }  
                     
-                    if(selector == 3){
+                    if(selector == WITHDRAW_CASH){
                         printf("Enter the sum: ");
                         scanf("%d", &operation);
                         if(operation <= client[i]){
@@ -90,7 +97,7 @@ int main() {
                         } else printf("There are not enough funds on your account\n");
                     }
                     
-                    if(selector == 4){
+                    if(selector == EXIT){
                         return 0;
                     }
                 }
